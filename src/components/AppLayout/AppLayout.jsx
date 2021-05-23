4c4163453670977b44d41c8f5b438e5f888d8366
jalpajora <jezamae.alpajora@gmail.com>
2021-05-23 17:16:38 +0800

    fix

--- src/components/AppLayout/AppLayout.jsx
+++ src/components/AppLayout/AppLayout.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import './styles.css';
 
-export default function MainLayout({ children }) {
-  const [header, content, footer] = children;
+export default function AppLayout({ children }) {
+
   return (
     <div className="app">
-      <header>
-        {header}
-      </header>
-      <main>
-        {content}
-      </main>
-      <footer>
-        {footer}
-      </footer>
+      {children}
     </div>
   );
 }
